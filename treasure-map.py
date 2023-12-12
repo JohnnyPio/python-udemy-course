21657af1ef623ed409d84e16fdbb4c65e95de5e9
John-Piotrowski <94871543+John-Piotrowski@users.noreply.github.com>
2023-12-12 18:52:43 -0500

    Updated treasure map based on Angela's much sleeker example

--- treasure-map.py
+++ treasure-map.py
@@ -5,34 +5,25 @@ map = [line1, line2, line3]
 print("Hiding your treasure! X marks the spot.")
 position = input() # Where do you want to put the treasure?
 # 🚨 Don't change the code above 👆
+
 # Write your code below this row 👇
-split_position = list(position)
-row_number_str = split_position[1]
-row_number = int(row_number_str)
-print(row_number)
-col_letter = split_position[0]
-col_number = 0
 
-if col_letter == "A":
-  col_number = 0
-elif col_letter == "B":
-  col_number = 1
-elif col_letter == "C":
-  col_number = 2
-else:
-  print("Column Error.")
-print(col_number)
+# This grabs the first element of the user input, the letter, and makes it lowercase
+letter = position[0].lower()
 
-if row_number == 1:
-  line1.pop(col_number)
-  line1.insert(col_number, "X")
-if row_number == 2:
-  line2.pop(col_number)
-  line2.insert(col_number, "X")
-if row_number == 3:
-  line3.pop(col_number)
-  line3.insert(col_number, "X")
+# This is a comparative list of letter options
+abc = ["a", "b", "c"]
+
+# This assigns an index number to the letter users entered
+letter_index = abc.index(letter)
+
+# This assigns an index number to the number users entered
+number_index = int(position[1]) - 1
+
+# This reassigns X using list-of-lists
+map[number_index][letter_index] = "X"
 
 # Write your code above this row 👆
+
 # 🚨 Don't change the code below 👇
 print(f"{line1}\n{line2}\n{line3}")
