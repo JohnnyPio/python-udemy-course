5cce517b665af9948b2b31c3454da0a2901c6b5c
JohnnyPio <john.s.piotrowski@gmail.com>
2023-12-19 17:53:13 -0500

    for-in loop for letter guess

--- hangman.py
+++ hangman.py
@@ -10,7 +10,8 @@ print(chosen_word)
 guess = input("Guess a letter.").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if chosen_word.find(guess) != -1:
-  print("hooray")
-else:
-  print("lose a life")
+for letter in chosen_word:
+    if letter == guess:
+        print("Right.")
+    else:
+        print("Wrong.")
