451ac38761457d090eb67e15cec87cf841eeb3a2
JohnnyPio <john.s.piotrowski@gmail.com>
2023-12-19 19:26:36 -0500

    Step 4 initial

--- hangman.py
+++ hangman.py
@@ -1,7 +1,69 @@
 import random
+
+# Stage ASCII art
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+# Variables
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_len = len(chosen_word)
+end_of_game = False
+lives = 6
 
 #Testing code
 print(f"pssst, the solution is {chosen_word}")
@@ -11,7 +73,7 @@ display = []
 for char in chosen_word:
     display += "_"
 
-end_of_game = False
+#Play Game
 while not end_of_game:
     guess = input("Guess a letter. ").lower()
 
@@ -20,8 +82,21 @@ while not end_of_game:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-    print(display)
+    
+    if chosen_word.find(guess) == -1:
+        lives -= 1
+    
+    if lives == 0:
+        end_of_game = True
+        print("You lose.")
 
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You've won!")
+
+    #Print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
