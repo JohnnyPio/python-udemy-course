418e24c8e07533dcae2c211640d0e615e3e394d2
JohnnyPio <john.s.piotrowski@gmail.com>
2023-12-22 09:49:27 -0500

    Error handling

--- function_with_outputs.py
+++ function_with_outputs.py
@@ -1,6 +1,8 @@
 # Function with Outputs
-
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        print("Must have non-empty names.")
+        return
     return f_name.title() + " " + l_name.title()
 
-print(format_name("BoB", "JuliaNO"))
+print(format_name("",""))
