38b2ca614bcff332ec9209ee2f516be1134d9e61
JohnnyPio <john.s.piotrowski@gmail.com>
2023-12-22 10:07:10 -0500

    Docstrings for format_name!!

--- function_with_outputs.py
+++ function_with_outputs.py
@@ -1,8 +1,13 @@
 # Function with Outputs
 def format_name(f_name, l_name):
+    #Docstring
+    """Take a first and last name and format it 
+    to return the title case version of the name."""
     if f_name == "" or l_name == "":
         print("Must have non-empty names.")
         return
     return f_name.title() + " " + l_name.title()
 
 print(format_name("",""))
+
+
