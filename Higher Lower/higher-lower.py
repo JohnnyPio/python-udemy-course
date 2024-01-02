53b45b2f748d122e2a012c71914c4e711f9ad4ab
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-02 12:22:30 -0500

    Still buggy

--- Higher Lower/higher-lower.py
+++ Higher Lower/higher-lower.py
@@ -14,18 +14,24 @@ remaining_samples = game_data.data
 
 # Prompt user which of the entries has a higher follower count
 def compare(fir_item,sec_item):
+    global current_score
     print(f"Compare A: {fir_item['name']}, a {fir_item['description']}, from {fir_item['country']}.")
     print(art.vs)   
     print(f"Compare B: {sec_item['name']}, a {sec_item['description']}, from {sec_item['country']}.")
-    choice = input("Who has more followers? Type 'A' or 'B':")
+    choice = input("Who has more followers? Type 'A' or 'B': ")
     if fir_item['follower_count'] > sec_item['follower_count'] and choice == "A":
         print("Correct")
+        current_score += 1
+        print(f"Current Score = {current_score}")
         return fir_item
     elif fir_item['follower_count'] < sec_item['follower_count'] and choice == "B":
         print("Correct")
+        current_score += 1
+        print(f"Current Score = {current_score}")
         return sec_item
     else:
         print("You lose.")
+        print(f"Final Score = {current_score}")
         return False
 
 # clear console and print logo after each game
@@ -40,16 +46,19 @@ remaining_samples.remove(first_item)
 second_item = random.choice(remaining_samples)
 remaining_samples.remove(second_item)
 
-# Get initial guess
-guess_check = compare(first_item,second_item)
-
 # (Loop) If right, pull next entry from data and remove from list
+guess_check = True
 while guess_check != False:
+    # Current score
+    current_score = 0
+    
+    # Get initial guess
+    guess_check = compare(first_item,second_item)
+
+    if guess_check == False:
+        break
+    
     new_item = random.choice(game_data.data)
     remaining_samples.remove(new_item)
 
-    new_guess_check = compare(guess_check, new_item)
-
-    # Compare those two
-    if guess_check == False:
-        break
+    guess_check = compare(guess_check, new_item)
