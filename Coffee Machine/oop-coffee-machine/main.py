1c6146ddc25db6cee8c1145dc3cae5bad8283421
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-04 13:34:25 -0500

    Initial OOP coffe machine

--- Coffee Machine/oop-coffee-machine/main.py
+++ Coffee Machine/oop-coffee-machine/main.py
@@ -2,3 +2,33 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_made = MoneyMachine()
+
+coffee_machine = True
+while coffee_machine:
+    # Prompt a user which coffee they want
+    user_choice = input(f"What coffee would you like? {menu.get_items()}.\n")
+
+    # Turn off coffee machine by selecting off
+    if user_choice == "off":
+        coffee_machine = False
+        print("Goodbye coffee...")
+        break
+
+    # Print Report
+    if user_choice == "report":
+        print(CoffeeMaker.report(coffee_maker))
+        user_choice = input(f"What coffee would you like? {menu.get_items()}.\n")
+
+    # Find drink in dict
+    selected_drink = menu.find_drink(user_choice)
+
+    # Check if resources are sufficient
+    if coffee_maker.is_resource_sufficient(selected_drink):
+        # Check if payment is sufficient
+        if money_made.make_payment(selected_drink.cost):
+            coffee_maker.make_coffee(selected_drink)
+            print(f"The total machine profit = {money_made.profit}")
+
