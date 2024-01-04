0cf01095d33a33e30d412eee8cd7239fe8631d65
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-04 10:10:56 -0500

    First package import!

--- day-16-start/main.py
+++ day-16-start/main.py
@@ -1,15 +1,18 @@
-# import another_module
-# print(another_module.another_variable)
+# # import another_module
+# # print(another_module.another_variable)
+#
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkGreen")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+#
+# my_screen.exitonclick()
 
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("DarkGreen")
-timmy.forward(100)
+import prettytable
 
-my_screen = Screen()
-print(my_screen.canvheight)
-
-my_screen.exitonclick()
 
