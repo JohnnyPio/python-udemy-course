46f264d77faf70c1684c8ad87f266c94002a60b6
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-06 18:08:05 -0500

    Draw a square with larger turtle

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -1,11 +1,14 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("HotPink")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
+tim = Turtle()
+tim.shape("turtle")
+tim.color("HotPink")
+tim.shapesize(5)
+
+# Draw a Square
+for num in range(4):
+    tim.forward(100)
+    tim.right(90)
 
 screen = Screen()
 screen.exitonclick()
