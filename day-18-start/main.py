e08ac26ce612a266e95de0bb56440e4d7c2d9c54
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-06 18:25:13 -0500

    # Draw a triangle through decagon

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -13,11 +13,17 @@ tim.shapesize(2)
 #     tim.right(90)
 
 # Draw a Dashed Line
-for _ in range(10):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+# for _ in range(10):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+# Draw a triangle through decagon
+for num in range(3, 10+1):
+    for _ in range(num):
+        tim.forward(100)
+        tim.right(360/num)
 
 screen = t.Screen()
 screen.exitonclick()
