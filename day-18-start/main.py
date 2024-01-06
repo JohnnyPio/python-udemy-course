805822b9e5c99041138a19b8acec858b7d5f4c6e
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-06 18:56:07 -0500

    Draw a random walk with random colors at a faster speed

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -6,9 +6,13 @@ import random
 tim = t.Turtle()
 tim.shape("turtle")
 tim.color("HotPink")
-tim.shapesize(2)
+tim.shapesize(3)
 #Set the global color mode
 t.colormode(255)
+#Use a thicker line size
+tim.pensize(20)
+#Move faster
+tim.speed(10)
 
 # Draw a Square
 # for _ in range(4):
@@ -23,18 +27,24 @@ t.colormode(255)
 #     tim.pendown()
 
 # Draw a triangle through decagon
-for num in range(3, 10+1):
-    red = random.randint(0, 255)
-    print(f"red = {red}")
-    green = random.randint(0, 255)
-    print(f"green = {green}")
-    blue = random.randint(0, 255)
-    print(f"blue = {blue}")
-    for _ in range(num):
-        tim.pencolor(red, green, blue)
-        tim.forward(100)
-        tim.right(360/num)
+# for num in range(3, 10+1):
+#     red = random.randint(0, 255)
+#     print(f"red = {red}")
+#     green = random.randint(0, 255)
+#     print(f"green = {green}")
+#     blue = random.randint(0, 255)
+#     print(f"blue = {blue}")
+#     for _ in range(num):
+#         tim.pencolor(red, green, blue)
+#         tim.forward(100)
+#         tim.right(360/num)
 
+# Draw a random walk with random colors
+turn_angles = [0, 90, 180, 270]
+for _ in range(200):
+    tim.pencolor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    tim.left(random.choice(turn_angles))
+    tim.forward(50)
 
 screen = t.Screen()
 screen.exitonclick()
