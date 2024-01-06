0deb342cd96b30f3187e4adcb56ee158d0981da9
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-06 18:40:58 -0500

    # Draw a triangle through decagon with different colors

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -1,11 +1,14 @@
 import turtle
 import turtle as t
+import random
 
 
 tim = t.Turtle()
 tim.shape("turtle")
 tim.color("HotPink")
 tim.shapesize(2)
+#Set the global color mode
+t.colormode(255)
 
 # Draw a Square
 # for _ in range(4):
@@ -21,9 +24,17 @@ tim.shapesize(2)
 
 # Draw a triangle through decagon
 for num in range(3, 10+1):
+    red = random.randint(0, 255)
+    print(f"red = {red}")
+    green = random.randint(0, 255)
+    print(f"green = {green}")
+    blue = random.randint(0, 255)
+    print(f"blue = {blue}")
     for _ in range(num):
+        tim.pencolor(red, green, blue)
         tim.forward(100)
         tim.right(360/num)
+
 
 screen = t.Screen()
 screen.exitonclick()
