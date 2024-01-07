f65f453a5753b848239f7c7b24ecebd62b70d666
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-07 17:52:22 -0500

    Initial Spacing in starting line

--- Daily Practice/day-19/main.py
+++ Daily Practice/day-19/main.py
@@ -1,13 +1,20 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
+width = 500
+height = 400
+screen.setup(width=width, height=height)
+user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race. Enter a color:")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
+starting_x = -width/2 + (width/20)
+starting_y = -height/2 + (height/12)
 
-def move_forwards():
-    tim.forward(10)
+for color in colors:
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(color)
+    new_turtle.goto(starting_x, starting_y)
+    starting_y += height/6
 
-
-screen.listen()
-screen.onkey(move_forwards, "space")
 screen.exitonclick()
