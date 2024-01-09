7f6019b822e955448f2ae64707be620a212228d6
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-08 20:54:32 -0500

    It's moving and turning!

--- Fun Stuff/Snake Game/main.py
+++ Fun Stuff/Snake Game/main.py
@@ -15,10 +15,11 @@ turtle.tracer(0)
 
 snake = Snake()
 
-screen.listen(snake.up,"Up")
-screen.listen(snake.down, "Down")
-screen.listen(snake.left,"Left")
-screen.listen(snake.right,"Right")
+screen.listen()
+screen.onkey(snake.up(), "Up")
+screen.onkey(snake.down,  "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
