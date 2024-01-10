ca4e7d31078114ef1890d39e154c0180733b941e
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-09 20:34:02 -0500

    Detect collision

--- Fun Stuff/Snake Game/main.py
+++ Fun Stuff/Snake Game/main.py
@@ -32,4 +32,9 @@ while game_is_on:
     time.sleep(.1)
     snake.move()
 
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+
 screen.exitonclick()
