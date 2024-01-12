44e0eb5ca817231f4cb17b6fa142488af3e6639d
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-11 19:24:47 -0500

    More main stuff

--- Fun Stuff/Pong/main.py
+++ Fun Stuff/Pong/main.py
@@ -14,17 +14,24 @@ screen.title("Pong")
 turtle.colormode(255)
 turtle.tracer(0)
 
-# Initialize
-#left_paddle = Paddle().create_left_paddle()
+# Initialize paddles
+left_paddle = Paddle((-350, 0))
+right_paddle = Paddle((350, 0))
 
 # Need a way to identify top/bot boundaries so ball will bounce
 # Need a way to identy left/right boundaries so scoring will count
 
-# Create a class for the paddles
 screen.listen()
-# screen.onkey(left_paddle.move_up, "Up")
-# screen.onkey(left_paddle.move_down, "Down")
+screen.onkey(left_paddle.go_up, "Up")
+screen.onkey(left_paddle.go_down, "Down")
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
 # Need a way to bounce ball off paddle
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 # Create a class for the ball
 
