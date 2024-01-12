ceb81f989d5d0b8b25c123dd7e100191db6e1242
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-11 19:37:07 -0500

    Better ball class

--- Fun Stuff/Pong/ball.py
+++ Fun Stuff/Pong/ball.py
@@ -3,14 +3,16 @@ from turtle import Turtle
 MOVE_DISTANCE = 20
 
 
-class Paddle(Turtle):
-    def __init__(self, position):
+class Ball(Turtle):
+    def __init__(self):
         super().__init__()
-        self.shape("square")
-        self.penup()
+        self.shape("circle")
         self.fillcolor("white")
-        self.goto(0, 400)
+        self.penup()
         self.setheading(225)
 
+    def move(self):
+        self.forward(MOVE_DISTANCE)
 
     # def bounce(self):
+    #     self.setheading(45)
