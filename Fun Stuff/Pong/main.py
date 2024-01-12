4a1c7728d3a18fc7988462bef5ae57c0671a05e4
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-11 19:38:01 -0500

    Working main-ish, need collisions

--- Fun Stuff/Pong/main.py
+++ Fun Stuff/Pong/main.py
@@ -1,6 +1,8 @@
 import turtle
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 # Create the screen, black background
 screen = Screen()
@@ -14,23 +16,27 @@ screen.title("Pong")
 turtle.colormode(255)
 turtle.tracer(0)
 
-# Initialize paddles
+# Initialize paddles and ball
 left_paddle = Paddle((-350, 0))
 right_paddle = Paddle((350, 0))
+ball = Ball()
 
 # Need a way to identify top/bot boundaries so ball will bounce
 # Need a way to identy left/right boundaries so scoring will count
 
+# Listeners for paddles
 screen.listen()
 screen.onkey(left_paddle.go_up, "Up")
 screen.onkey(left_paddle.go_down, "Down")
-screen.onkey(right_paddle.go_up, "Up")
-screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(right_paddle.go_up, "w")
+screen.onkey(right_paddle.go_down, "s")
 # Need a way to bounce ball off paddle
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 # Create a class for the ball
