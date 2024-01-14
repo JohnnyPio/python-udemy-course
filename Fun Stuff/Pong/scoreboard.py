a509b48f88a66afeeeef5db909f797dcd508c9c6
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-13 19:30:41 -0500

    Initial scoreboard

--- Fun Stuff/Pong/scoreboard.py
+++ Fun Stuff/Pong/scoreboard.py
@@ -1,13 +1,13 @@
 from turtle import Turtle
 
-ALIGNMENT = "center"
 FONT = ("Courier", 16, "normal")
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.left_paddle_score = 0
+        self.right_paddle_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 275)
@@ -16,12 +16,13 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"{self.left_paddle_score} to {self.right_paddle_score}", align="left", font=FONT)
 
-    def increase_score(self):
-        self.score += 1
+    def increase_left_score(self):
+        self.left_paddle_score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def increase_right_score(self):
+        self.right_paddle_score += 1
+        self.update_scoreboard()
+
