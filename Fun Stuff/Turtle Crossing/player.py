c76c6ac088090433cbaace22a3d80ca75113e785
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-15 19:41:59 -0500

    Good start to the player file

--- Fun Stuff/Turtle Crossing/player.py
+++ Fun Stuff/Turtle Crossing/player.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.fillcolor("black")
+        self.setheading(90)
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.y_move = MOVE_DISTANCE
+        self.move_speed = 0.1
+
+    def move(self):
+        new_y = self.ycor() + self.y_move
+        self.goto(STARTING_POSITION[0], new_y)
