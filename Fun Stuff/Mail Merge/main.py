9a7380b66de96e33e036d7a26044ff9d13b2c8b1
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-16 19:54:50 -0500

    Initial working mail merge!

--- Fun Stuff/Mail Merge/main.py
+++ Fun Stuff/Mail Merge/main.py
@@ -1,5 +1,20 @@
-#TODO: Create a letter using starting_letter.txt 
+#TODO: Create a letter using starting_letter.txt
+all_names = []
+with open("Input/Names/invited_names.txt", "r") as names_file:
+    raw_read = names_file.readlines()
+    for names in raw_read:
+        all_names.append(names[:-1])
+
+
 #for each name in invited_names.txt
+with open("Input/Letters/starting_letter.txt", "r") as letter:
+    contents = letter.read()
+    for name in all_names:
+        new_letter = contents.replace("[name]", f"{name}")
+        new_file = open(f"Output/ReadyToSend/letter for {name}.txt", "w")
+        new_file.write(new_letter)
+        new_file.close()
+
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
     
