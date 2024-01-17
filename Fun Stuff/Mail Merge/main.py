1751e46ed7861b58dbc471aaddcfcda8bb69836a
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-16 19:57:12 -0500

    Used strip()...duh

--- Fun Stuff/Mail Merge/main.py
+++ Fun Stuff/Mail Merge/main.py
@@ -1,12 +1,10 @@
-#TODO: Create a letter using starting_letter.txt
 all_names = []
 with open("Input/Names/invited_names.txt", "r") as names_file:
     raw_read = names_file.readlines()
     for names in raw_read:
-        all_names.append(names[:-1])
+        all_names.append(names.strip())
 
 
-#for each name in invited_names.txt
 with open("Input/Letters/starting_letter.txt", "r") as letter:
     contents = letter.read()
     for name in all_names:
@@ -14,10 +12,3 @@ with open("Input/Letters/starting_letter.txt", "r") as letter:
         new_file = open(f"Output/ReadyToSend/letter for {name}.txt", "w")
         new_file.write(new_letter)
         new_file.close()
-
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
