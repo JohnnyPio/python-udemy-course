9b5ddb02de20634c0af645d4231db9e125a529fc
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-16 19:07:39 -0500

    Read/Write permanent high score to disk

--- Fun Stuff/Snake Game/scoreboard.py
+++ Fun Stuff/Snake Game/scoreboard.py
@@ -4,11 +4,17 @@ ALIGNMENT = "center"
 FONT = ("Courier", 16, "normal")
 
 
+def read_high_score():
+    with open("data.txt") as file:
+        contents = int(file.read())
+        return contents
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = read_high_score()
         self.color("white")
         self.penup()
         self.goto(0, 275)
@@ -26,5 +32,10 @@ class Scoreboard(Turtle):
     def reset_scoreboard(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.update_high_score()
         self.score = 0
         self.update_scoreboard()
+
+    def update_high_score(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.high_score))
