22d3d6c491487c7e076c2008601ebf573263ab0a
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-20 19:00:56 -0500

    More pandas, create CSV from dict

--- Daily Practice/day-25/main.py
+++ Daily Practice/day-25/main.py
@@ -10,5 +10,31 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(type(data["temp"]))
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+
+# avg = sum(temp_list)/len(temp_list)
+# print(data["temp"].mean())
+# print(data["temp"].max())
+# print(data.condition)
+
+# print(data[data.day == "Monday"])
+
+# max_temp_celsius = data[data.day == "Monday"]["temp"]
+# max_temp_f = 9/5 * max_temp_celsius + 32
+# print(max_temp_f)
+
+# Create a dict from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
