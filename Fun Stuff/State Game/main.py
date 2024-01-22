aa41b44e0618ec34ad2355c98dc90cabc4504702
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-22 14:05:28 -0500

    Incorporated list comprehension for missing states

--- Fun Stuff/State Game/main.py
+++ Fun Stuff/State Game/main.py
@@ -10,14 +10,13 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data["state"].to_list()
+guessed_states = []
 
-num_states_guessed = 0
-states_left = all_states
-
-while num_states_guessed < 50:
-    answer_state = screen.textinput(title=f"{num_states_guessed}/50 Guess the State",
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State",
                                     prompt="What's another state's name?").title()
     if answer_state in all_states:
+        guessed_states.append(answer_state)
         state_text = turtle.Turtle()
         state_text.hideturtle()
         state_text.penup()
@@ -25,15 +24,11 @@ while num_states_guessed < 50:
         state_text.goto(int(state_data.x), int(state_data.y))
         state_text.write(answer_state, align="center")
 
-    # Error Handling if the same state is guessed again
-    if answer_state in states_left:
-        states_left.remove(answer_state)
-        num_states_guessed += 1
-
     if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
 
-file_title = "states_to_learn.csv"
-new_data = pandas.DataFrame(states_left)
-new_data.to_csv(file_title)
+
 
