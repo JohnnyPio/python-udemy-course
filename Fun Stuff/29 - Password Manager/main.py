dc86fbc43657fdef2d840528c1d1473f0ca9aa2b
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-26 16:53:22 -0500

    PW's Writing to file :)

--- Fun Stuff/29 - Password Manager/main.py
+++ Fun Stuff/29 - Password Manager/main.py
@@ -1,5 +1,17 @@
 from tkinter import *
 
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_to_file():
+    with open("data.txt", "a") as file:
+        file.write(f"{website_input.get()} | {email_username_input.get()} | {password_input.get()}\n")
+        website_input.delete(0, END)
+        password_input.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 # Window setup
 window = Tk()
 window.title("Password Manager")
@@ -18,6 +30,7 @@ website_label.grid(row=website_row, column=0)
 
 website_input = Entry(width=35)
 website_input.grid(row=website_row, column=1, columnspan=2, sticky="EW")
+website_input.focus()
 
 # Email/Username Line
 email_username_row = website_row + 1
@@ -26,6 +39,7 @@ email_username_label.grid(row=email_username_row, column=0)
 
 email_username_input = Entry(width=35)
 email_username_input.grid(row=email_username_row, column=1, columnspan=2, sticky="EW")
+email_username_input.insert(END, "john.s.piotrowski@gmail.com")
 
 # Password Line
 password_row = website_row + 2
@@ -40,14 +54,7 @@ password_button.grid(row=password_row, column=2)
 
 # Add
 add_row = website_row + 3
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=add_to_file)
 add_button.grid(row=add_row, column=1, columnspan=2, sticky="EW")
-
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-
-# ---------------------------- UI SETUP ------------------------------- #
-
 
 window.mainloop()
