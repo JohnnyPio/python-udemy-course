65204b2b4304d5d2819a78322b87c8b88be77614
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-26 15:12:55 -0500

    Background image

--- Fun Stuff/29 - Password Manager/main.py
+++ Fun Stuff/29 - Password Manager/main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+# Icon
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+lock_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_image)
+canvas.pack()
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+window.mainloop()
