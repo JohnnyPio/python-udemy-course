65c216ac6e415d49f6f76020506d09e4dab1a5d8
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-28 18:11:03 -0500

    Raise our own exception

--- Daily Practice/day-30/main.py
+++ Daily Practice/day-30/main.py
@@ -15,18 +15,25 @@
 # print(text+5)
 
 # FileNotFound
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"That key {error_message} doesn't exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed.")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"That key {error_message} doesn't exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise KeyError("This is a madeup error.")
+
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3:
+    raise ValueError("This is too tall for human height.")
+bmi = weight / height ** 2
+print(bmi)
