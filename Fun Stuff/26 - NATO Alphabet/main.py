58ac5ea2a7a7e196a4dba2200ffd6325f0b7c1cc
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-28 18:33:11 -0500

    Improved NATO Alphabet with try/catch + recursion :))

--- Fun Stuff/26 - NATO Alphabet/main.py
+++ Fun Stuff/26 - NATO Alphabet/main.py
@@ -1,32 +1,18 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-#
-# # # Looping through dictionaries:
-# # for (key, value) in student_dict.items():
-# #     # Access key and value
-# #     pass
-
 import pandas
-
-# student_data_frame = pandas.DataFrame(student_dict)
-#
-# # Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     # Access index and row
-#     # Access row.student or row.score
-#     pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 # Create a dictionary in this format:
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
-user_word = input("Enter a word: ").upper()
-phonetic_list = [nato_dict[letter] for letter in user_word]
-print(phonetic_list)
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper()
+    try:
+        phonetic_list = [nato_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
 
+generate_phonetic()
