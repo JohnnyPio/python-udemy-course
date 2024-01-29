defd28b36f377d0da79b8d50de770dac5c97d4d6
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-29 10:06:10 -0500

    Minor stuff

--- Fun Stuff/26 - NATO Alphabet/main.py
+++ Fun Stuff/26 - NATO Alphabet/main.py
@@ -4,6 +4,7 @@ import pandas
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
+
 # Create a list of the phonetic code words from a word that the user inputs.
 def generate_phonetic():
     user_word = input("Enter a word: ").upper()
@@ -14,5 +15,6 @@ def generate_phonetic():
         generate_phonetic()
     else:
         print(phonetic_list)
+
 
 generate_phonetic()
