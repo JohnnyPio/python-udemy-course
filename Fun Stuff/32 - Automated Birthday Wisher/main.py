312fd0cc7a9a783c6abdc91d7234e9c348674948
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-31 13:10:21 -0500

    SMTP sending test email!

--- Fun Stuff/32 - Automated Birthday Wisher/main.py
+++ Fun Stuff/32 - Automated Birthday Wisher/main.py
@@ -4,8 +4,9 @@ my_email = "john.s.piotrowski@gmail.com"
 password = "mgqw gtcc fame vnjd"
 to_email = "northeastmanic1967@yahoo.com"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs=to_email, msg="hello, this is a test run.")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs=to_email,
+                        msg="Subject: Hello \n\n hello, this is a test run."
+                        )
