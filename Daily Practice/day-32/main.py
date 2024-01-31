426bd983d93343abde24e72d1a98bcc21f56fc22
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-31 15:03:47 -0500

    Moved stuff around

---
+++ Daily Practice/day-32/main.py
@@ -0,0 +1,14 @@
+# import smtplib
+#
+# my_email = "john.s.piotrowski@gmail.com"
+# password = "mgqw gtcc fame vnjd"
+# to_email = "Kristinlewis2012@gmail.com"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email, to_addrs=to_email,
+#                         msg="Subject: John's Python Course! \n\n Hi, this is me sending you an email from Python :)"
+#                         )
+
+import datetime
