c773e6efd997c52b85c718d2c986c16058690762
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-31 12:27:36 -0500

    Dictionaries need more practice.

--- Fun Stuff/31 - Flash Card App/main.py
+++ Fun Stuff/31 - Flash Card App/main.py
@@ -1,7 +1,7 @@
+import csv
 from tkinter import *
 import random
 import pandas
-import time
 
 # ---------------------------- CONSTANTS ------------------------------ #
 BACKGROUND_COLOR = "#B1DDC6"
@@ -10,7 +10,7 @@ NATIVE_LANGUAGE = "English"
 
 imported_data = pandas.read_csv("data/french_words.csv")
 to_learn = imported_data.to_dict(orient="records")
-learned = {}
+learned = []
 current_card = {}
 
 
@@ -32,14 +32,14 @@ def flip_card():
     canvas.itemconfig(card_language, text=NATIVE_LANGUAGE, fill="white")
     canvas.itemconfig(card_word, text=current_card[NATIVE_LANGUAGE], fill="white")
 
+
 def remove_card():
-    # if text isn't starter text
-
-    # when green button is pressed, remove it from to_learn and move it to learned
-
-    # print new CSV
-
-# need to check if new CSV exists
+    global current_card
+    to_learn.remove(current_card)
+    learned.append(current_card)
+    learned_df = pandas.DataFrame(learned)
+    learned_df.to_csv("words_to_learn.csv", index=False)
+    get_next_card()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -57,7 +57,7 @@ canvas_image = canvas.create_image(400, 263, image=front_image)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Card Labels TODO - clean this initializer up
-card_language = canvas.create_text(400, 150, text="Press Button to Start", font=("Ariel", 40, "italic"))
+card_language = canvas.create_text(400, 150, font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, font=("Ariel", 60, "bold"))
 
 # Buttons
@@ -67,7 +67,10 @@ wrong_button = Button(image=wrong_image, highlightthickness=0, bg=BACKGROUND_COL
 wrong_button.grid(row=button_row, column=0, sticky="EW")
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, command=get_next_card)
+right_button = Button(image=right_image, highlightthickness=0, bg=BACKGROUND_COLOR, command=remove_card)
 right_button.grid(row=button_row, column=1, sticky="EW")
+
+# Initialize cards
+get_next_card()
 
 window.mainloop()
