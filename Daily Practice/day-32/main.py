2b6a8662512fa7e2ec35d9f2bed350591b09fb51
JohnnyPio <john.s.piotrowski@gmail.com>
2024-01-31 20:10:46 -0500

    Motivation Monday quotes

--- Daily Practice/day-32/main.py
+++ Daily Practice/day-32/main.py
@@ -1,4 +1,7 @@
-# import smtplib
+import random
+import smtplib
+import datetime as dt
+
 #
 # my_email = "john.s.piotrowski@gmail.com"
 # password = "mgqw gtcc fame vnjd"
@@ -11,11 +14,37 @@
 #                         msg="Subject: John's Python Course! \n\n Hi, this is me sending you an email from Python :)"
 #                         )
 
-import datetime as dt
 
+#
+# now = dt.datetime.now()
+# day_of_week = now.weekday()
+# print(day_of_week)
+#
+# date_of_birth = dt.datetime(year=1990, month=7, day=23, hour=19)
+# print(date_of_birth)
+
+### ------ Monday Motivation ------ ###
+
+# Email setup
+my_email = "john.s.piotrowski@gmail.com"
+password = "mgqw gtcc fame vnjd"
+to_email = "Kristinlewis2012@gmail.com"
+
+# Date setup
 now = dt.datetime.now()
 day_of_week = now.weekday()
-print(day_of_week)
 
-date_of_birth = dt.datetime(year=1990, month=7, day=23, hour=19)
-print(date_of_birth)
+#
+if day_of_week == 0:
+    with open("quotes.txt", "r", encoding="utf8") as file:
+        all_quotes = file.readlines()
+        random_quote = random.choice(all_quotes)
+        print(random_quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg=f"Subject: Monday Motivation! \n\n {random_quote}".encode("utf8")
+        )
