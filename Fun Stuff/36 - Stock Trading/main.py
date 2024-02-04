a65935843015aa2f2980b611389d0779ce9f78c7
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-04 16:29:54 -0500

    Got Step 2

--- Fun Stuff/36 - Stock Trading/main.py
+++ Fun Stuff/36 - Stock Trading/main.py
@@ -6,38 +6,41 @@ import requests
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
+NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
 
-url = "https://www.alphavantage.co/query"
+
+stock_url = "https://www.alphavantage.co/query"
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": "TSLA",
     "apikey": STOCK_API_KEY
 }
 
-# response = requests.get(url, params=stock_params)
-# stock_data = response.json()
-# historical_data = stock_data.get("Time Series (Daily)")
-# data_df = pandas.DataFrame.from_dict(historical_data)
+news_url = "https://newsapi.org/v2/top-headlines"
+news_params = {
+    "apiKey": NEWS_API_KEY,
+    "q": "IBM",
+}
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo").json()
 historical_data = response.get("Time Series (Daily)")
-# print(historical_data)
 first_key = next(iter(historical_data))
-most_recent_open = float(historical_data.get(first_key).get("1. open"))
+most_recent_close = float(historical_data.get(first_key).get("4. close"))
 for key in historical_data:
     current_day_open = float(historical_data.get(key).get("1. open"))
-    try:
-        if (abs(current_day_open - most_recent_open) / most_recent_open >= 0.05):
-            print(f"get news for {key}")
-    except:
-        print("No 5% swings")
-    finally:
-        most_recent_open = current_day_open
+    if abs(current_day_open - most_recent_close) / most_recent_close >= 0.05:
+        ## STEP 2: Use https://newsapi.org
+        # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+        news_response = requests.get(news_url, news_params)
+        news = news_response.json().get("articles")
+        for new in news:
+            title = new.get("title")
+            print(title)
+    current_day_close = float(historical_data.get(key).get("4. close"))
+    most_recent_close = current_day_close
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
