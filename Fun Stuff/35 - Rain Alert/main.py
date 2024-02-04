24a202160ca0ab7fc4d9f7a72bcdf993044be359
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-04 09:45:11 -0500

    315 unoptimized

--- Fun Stuff/35 - Rain Alert/main.py
+++ Fun Stuff/35 - Rain Alert/main.py
@@ -6,9 +6,24 @@ WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 weather_params = {
     "lat": 40.485489,
     "lon": -106.833557,
+    "cnt": 4,
+    "units": "imperial",
     "appid": API_KEY,
 }
 
+response = requests.get(WEATHER_ENDPOINT, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
 
-response = requests.get(WEATHER_ENDPOINT, params=weather_params).json()
-print(response)
+# Check for rain
+ids = []
+for item in weather_data.get("list"):
+    weather_list_item = item.get("weather")
+    for hourly_weather in weather_list_item:
+        hourly_id = hourly_weather.get("id")
+        ids.append(hourly_id)
+
+if [i for i in ids if i < 600]:
+    print("Bring an umbrella")
+else:
+    print("You good")
