66684b7eff4030d0c9e2489b782aa2da4d31ae11
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-04 14:42:05 -0500

    Cutting my Twilio losses.

--- Fun Stuff/35 - Rain Alert/main.py
+++ Fun Stuff/35 - Rain Alert/main.py
@@ -1,8 +1,12 @@
 import requests
 import os
+from twilio.rest import Client
 
 API_KEY = os.environ.get("OWM_API_KEY")
 WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+TWILIO_USER_SID = "US001af0175f580d24185585021e5d6ea5"
+AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_NUMBER = "+18442647034"
 
 weather_params = {
     "lat": 40.485489,
@@ -24,7 +28,13 @@ for item in weather_data.get("list"):
         hourly_id = hourly_weather.get("id")
         ids.append(hourly_id)
 
-if [i for i in ids if i < 600]:
-    print("Bring an umbrella")
-else:
-    print("You good")
+# if [i for i in ids if i < 600]:
+#
+#     )
+client = Client(TWILIO_USER_SID, AUTH_TOKEN)
+message = client.messages.create(
+    body="It's going to rain.",
+    from_=TWILIO_NUMBER,
+    to="+16092174019"
+)
+print(message.status)
