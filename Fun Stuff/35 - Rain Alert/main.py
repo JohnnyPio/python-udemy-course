876fed048b427210fdd045ed9928c83756c0349b
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-04 09:16:01 -0500

    WE back up!

--- Fun Stuff/35 - Rain Alert/main.py
+++ Fun Stuff/35 - Rain Alert/main.py
@@ -1,8 +1,14 @@
 import requests
 
 API_KEY = "2ecab42cfaf25fd97f8fe4d2acba44cf"
-MY_LAT = 40.485489
-MY_LONG = -106.833557
+WEATHER_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 
-response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={MY_LAT}&lon={MY_LONG}&appid={API_KEY}").json
+weather_params = {
+    "lat": 40.485489,
+    "lon": -106.833557,
+    "appid": API_KEY,
+}
+
+
+response = requests.get(WEATHER_ENDPOINT, params=weather_params).json()
 print(response)
