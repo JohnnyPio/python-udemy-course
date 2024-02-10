00ee3bdf5924f8f65de26f9895724f51a64da8bc
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-10 15:35:09 -0500

    Pixela up and running with POSTs

---
+++ Fun Stuff/37 - Habit Tracker/main.py
@@ -0,0 +1,48 @@
+import requests
+import os
+
+USERNAME = "jspiotrowski"
+TOKEN = os.environ.get("PIXELA_TOKEN")
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Phoebe Walks",
+    "unit": "walks",
+    "type": "int",
+    "color": "kuro",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# print(response.text)
+
+# Posting some data
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_params = {
+    "date": "20240209",
+    "quantity": "1",
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_params, headers=headers)
+print(response.text)
+
+
