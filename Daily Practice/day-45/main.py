3f68e94a8a1d22b3ecc184a729c15ce30f772f07
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-19 17:17:54 -0500

    Lesson 374

--- Daily Practice/day-45/main.py
+++ Daily Practice/day-45/main.py
@@ -8,3 +8,22 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.title.string)
 # print(soup.prettify())
 # print(soup.p)
+
+all_anchor_tags = soup.find_all(name="a")
+for tag in all_anchor_tags:
+    print(tag.get_text())
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.get_text())
+
+company_url = soup.select_one(selector="p a").get("href")
+print(company_url)
+
+name = soup.select_one(selector="#name")
+print(name)
+
+print(soup.select(".heading"))
