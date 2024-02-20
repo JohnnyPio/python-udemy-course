5228dd7cd803acf71f619180a766d34e479e60af
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-20 15:53:55 -0500

    100 Top Movies as a List :)

--- Fun Stuff/45 - 100 Movies Website Scraper/main.py
+++ Fun Stuff/45 - 100 Movies Website Scraper/main.py
@@ -1,5 +1,16 @@
 from bs4 import BeautifulSoup
-import lxml
+import requests
 
-with open("website.html", "r", encoding="utf8") as file:
-    contents = file.read()
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/"
+                        "best-movies-2/")
+response.encoding = "utf-8"
+movie_webpage = response.text
+soup = BeautifulSoup(movie_webpage, "html.parser")
+
+all_title_text = soup.find_all(class_="article-title-description__text")
+all_title_raw_text = [item.find("h3").get_text() for item in all_title_text]
+all_title_raw_text.reverse()
+
+with open("movies.txt", "wt", encoding="utf-8") as file:
+    for item in all_title_raw_text:
+        file.write(item+'\n')
