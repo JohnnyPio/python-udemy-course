78ff566fda17009692431ce0f93ae19faf498489
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-21 16:12:38 -0500

    Lesson 391 - Python + Return

--- Daily Practice/day-48/interaction.py
+++ Daily Practice/day-48/interaction.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # Keep Chrome open
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
+driver.maximize_window()
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/a[1]').text
-print(article_count)
+article_count = driver.find_element(By.XPATH, value='//*[@id="articlecount"]/a[1]')
+# article_count.click()
 
-driver.quit()
+search = driver.find_element(By.NAME, value="search")
+search.send_keys("Python", Keys.RETURN)
+
+# driver.quit()
