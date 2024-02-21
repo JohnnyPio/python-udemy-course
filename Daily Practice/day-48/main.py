16ec31c0f0f7faee349c96f223f8d6f9d09037db
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-21 11:49:27 -0500

    Init selenium open/close

---
+++ Daily Practice/day-48/main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+# Keep Chrome open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://amazon.com")
+
+driver.quit()
