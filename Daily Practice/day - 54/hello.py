ff1fd223c6328c7876a69fbac9554c35ce8ae412
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-23 17:27:12 -0500

    422

--- Daily Practice/day - 54/hello.py
+++ Daily Practice/day - 54/hello.py
@@ -1,8 +1,12 @@
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
 
+# @app.route("/")
+# def hello_world():
+#     return "Hello, World! 2"
 
-@app.route("/")
-def hello_world():
-    return "Hello, World! 2"
+if __name__ == "__main__":
+    app.run()
