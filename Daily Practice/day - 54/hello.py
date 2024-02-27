6f9fc6ff3731b085a4a7a233563f8d2f7e599913
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-27 10:22:53 -0500

    Lesson 428

--- Daily Practice/day - 54/hello.py
+++ Daily Practice/day - 54/hello.py
@@ -7,15 +7,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return ('<h1 style="text-align: center">Hello, World!</h1>'
+            '<p>This is a paragraph</p>'
+            '<img src="https://media.giphy.com/media/'
+            'v1.Y2lkPTc5MGI3NjExNWw4dmdyZGk5ZTBqYjF2MGx0bm1jNzJ4NG9oa3RhM3h6NWlwajR2cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UB9x43ZH3105v70743/giphy.gif">')
 
 
+# Different routes using the app.route decorator
 @app.route("/bye")
 def say_bye():
     return "Bye"
 
 
-@app.route("/<name>")
+# Creating variable paths and converting the path
+@app.route("/<name>/1")
 def greet(name):
     return f"Hello {name}!"
 
