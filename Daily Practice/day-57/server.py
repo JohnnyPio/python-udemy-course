9d2fae21f9a06533fd9bf24f3076ae3b97fdcb13
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-28 18:49:34 -0500

    Lesson 440

--- Daily Practice/day-57/server.py
+++ Daily Practice/day-57/server.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import datetime
+import random
 
 app = Flask(__name__)
+
+my_name = "Johnny Pio"
+current_year = datetime.date.today().year
+print(current_year)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    rand_num = random.randint(1, 10)
+    return render_template("index.html", my_rand_num=rand_num, MY_NAME=my_name, CURRENT_YEAR=current_year)
 
 
 if __name__ == "__main__":
