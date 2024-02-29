1f733b391e8131f23d862d1fa6e0d07fcf2c1034
JohnnyPio <john.s.piotrowski@gmail.com>
2024-02-28 20:12:51 -0500

    Lesson 442 - Multiline Jinja statements

--- Daily Practice/day-57/server.py
+++ Daily Practice/day-57/server.py
@@ -35,5 +35,13 @@ def guessing(name):
     return render_template("index.html", YOUR_NAME=name, YOUR_GENDER=your_gender, YOUR_AGE=your_age)
 
 
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
